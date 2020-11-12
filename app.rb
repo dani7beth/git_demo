22dfff051fbe89b10d4151db8f6c76ea61cc5ece
dani7beth <danielledona28@gmail.com>
2020-11-12 11:16:44 -0700

    menu class now takes hash and prints menu

--- app.rb
+++ app.rb
@@ -1,5 +1,39 @@
 require_relative 'menu.rb'
 
-menu_test = Menu.new
+default_menu_hash ={ 
+    message: "Please Select an option",
+    options: ['option 1', 'option 2', 'exit']
+}
+menu2 ={
+    message: "Welcome to the Casino select a game",
+    options: ['Craps', 'Poker', 'High/Low', 'exit']
+}
 
-puts menu_test.class
+menu_test = Menu.new(default_menu_hash)
+menu_test.print
+
+casino_menu = Menu.new(menu2)
+casino_menu.print
+# please select an option
+# 1) option 1
+# 2) option 2
+# 3) exit
+
+
+# generally have a message Please select an option/please choose 
+# list of options
+# user selecting one of those
+
+#i want to list a menu using my class
+
+
+# what kind of data structure does that look like??
+# a hash since it will have an option and a message
+
+
+
+# menu1 ={
+#     message: "Please Select an option" 
+#     options: ['add contact', 'update contact', 'list contact', 'exit']
+# }
+
--- menu.rb
+++ menu.rb
@@ -1,4 +1,21 @@
 class Menu
-    def initialize
+    def initialize(menu)
+        @menu = menu
+    end
+
+    #define print method
+    def print
+        # this @menu looks like
+        # {:message=>"Please Select an option", :options=>["option 1", "option 2", "exit"]}
+
+        #display menu message
+        puts @menu[:message]
+        puts "-------------"
+
+        #loop over menu options, need to add logic to add 1) as well
+        @menu[:options].each_with_index do |item, index|
+            puts "#{index+1}). #{item}"
+        end
+        puts " "
     end
 end
